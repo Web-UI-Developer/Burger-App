8603d0de86d57549b4a1e37e84fbd701803641b9
Web-UI-Developer <sampreet.github@gmail.com>
2019-02-17 18:31:19 -0600

    Implemented React-readux

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL:'https://burger-app-acd79.firebaseio.com/'
+    baseURL: 'https://react-my-burger.firebaseio.com/'
 });
-  
+
 export default instance;
